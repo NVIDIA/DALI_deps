328bb3f2513e0abe609ed0e944ac40d3aedd6e26
Joaquin Anton <janton@nvidia.com>
2021-05-18 15:34:00 +0200

    Workaround missing tag when using a shallow copy of DALI_deps
    Signed-off-by: Joaquin Anton <janton@nvidia.com>

--- build_scripts/build_libopus.sh
+++ build_scripts/build_libopus.sh
@@ -14,8 +14,18 @@
 # See the License for the specific language governing permissions and
 # limitations under the License.
 
+export ROOT_DIR=$(pwd)
+
 # libopus
 pushd third_party/opus
+
+# Configure step relies on the git tag to generate the version
+# Since we only have a shallow copy of the submodule, without tags, the version can't be determined
+# This is a workaround, parsing the README file to get the tag used, and create a tag in the local repo
+# before calling configure
+OPUS_VERSION=$(grep https://github.com/xiph/opus/releases/tag ${ROOT_DIR}/README.rst  | grep -Eo 'v[0-9]+.[0-9]+.[0-9]+')
+git tag $OPUS_VERSION || true
+
 ./autogen.sh
 ./configure \
     CFLAGS="-fPIC" \
@@ -24,6 +34,7 @@ pushd third_party/opus
     CXX=${CXX_COMP} \
     ${HOST_ARCH_OPTION} \
     --prefix=${INSTALL_PREFIX}
+
 make -j"$(grep ^processor /proc/cpuinfo | wc -l)"
 make install
 popd
