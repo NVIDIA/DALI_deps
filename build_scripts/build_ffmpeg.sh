bbd2bd688b9d620e5a6dbe90549df5a4917fe6b1
Janusz Lisiecki <jlisiecki@nvidia.com>
2021-06-14 13:36:37 +0200

    Fix CVE-2021-33815 in FFmpeg
    Signed-off-by: Janusz Lisiecki <jlisiecki@nvidia.com>

--- build_scripts/build_ffmpeg.sh
+++ build_scripts/build_ffmpeg.sh
@@ -17,6 +17,14 @@
 # For a snapshot of the code, see the README.rst
 if [ ${WITH_FFMPEG} -gt 0 ]; then
     pushd third_party/FFmpeg
+    echo "--- a/libavcodec/exr.c
++++ b/libavcodec/exr.c
+@@ -1062 +1062 @@ static int dwa_uncompress(EXRContext *s, const uint8_t *src, int compressed_size
+-    if (dc_size > 0) {
++    {
+@@ -1066 +1066 @@ static int dwa_uncompress(EXRContext *s, const uint8_t *src, int compressed_size
+-        if (dc_count > (6LL * td->xsize * td->ysize + 63) / 64)
++        if (dc_count != dc_w * dc_h * 3)" | patch -p 1
     ./configure \
         --prefix=${INSTALL_PREFIX} \
         --disable-static \
