a0c0e0cb02d159b0bd83cfd51dfa84158df9196d
Krzysztof Lecki <klecki@nvidia.com>
2022-02-21 15:10:24 +0100

    Fix libtiff-CVE-2022-0561 (#39)
    Signed-off-by: Krzysztof Lecki <klecki@nvidia.com>

--- build_scripts/build_libtiff.sh
+++ build_scripts/build_libtiff.sh
@@ -18,6 +18,7 @@
 pushd third_party/libtiff
 patch -p1 < ${ROOT_DIR}/patches/0001-Fix-wget-complaing-about-expired-git.savannah.gnu.or.patch
 patch -p1 < ${ROOT_DIR}/patches/libtiff-CVE-2022-22844.patch
+patch -p1 < ${ROOT_DIR}/patches/libtiff-CVE-2022-0561.patch
 ./autogen.sh
 ./configure \
     CFLAGS="-fPIC" \
