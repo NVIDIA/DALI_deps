26eef9a9b4da6e9847a41d231f531a883f842d14
Joaquin Anton <janton@nvidia.com>
2024-04-15 17:48:38 +0200

    Code review fixes
    Signed-off-by: Joaquin Anton <janton@nvidia.com>

--- build_scripts/build_aws-sdk-cpp.sh
+++ build_scripts/build_aws-sdk-cpp.sh
@@ -61,7 +61,7 @@ case "$CMAKE_TARGET_ARCH" in
 esac
 CC=${CC_COMP} CXX=${CXX_COMP}" ${CPPFLAGS} ${CFLAGS}" \
   ${_CONFIGURATOR} ${OPTS[@]} ${LDFLAGS}
-make install libcrypto.a libssl.a
+make install libcrypto.a libssl.a -j"$(grep ^processor /proc/cpuinfo | wc -l)"
 
 popd
 # OpenSSL done
